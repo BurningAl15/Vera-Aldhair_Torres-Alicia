26a1f63ef9b515f1de90cd9c940b53ff34c25f3b
Aldhair Vera Camacho <aldhairvera15@gmail.com>
2024-05-15 21:29:23 -0500

    Odontologo base

---
+++ Clase 11- Taller Coding/C2ClinicaOdontologicaC11/src/dao/OdontologoDaoH2.java
@@ -0,0 +1,32 @@
+package dao;
+
+import model.Odontologo;
+
+import java.util.List;
+
+public class OdontologoDaoH2 implements iDao<Odontologo> {
+    @Override
+    public Odontologo guardar(Odontologo odontologo) {
+        return null;
+    }
+
+    @Override
+    public Odontologo buscarPorID(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void actualizar(Odontologo odontologo) {
+
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+
+    }
+
+    @Override
+    public List<Odontologo> buscarTodos() {
+        return null;
+    }
+}
